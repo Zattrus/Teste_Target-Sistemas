09e6b96bff16f16edff65da54bc292ab742d997c
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2022-06-27 20:13:28 -0300

    Codigo para inverter uma 'string'

---
+++ string.js
@@ -0,0 +1,13 @@
+// Teste 5
+
+// EXEMPLO DE ENTRADA:
+var string = "string";
+var stringInvertida = "";
+
+
+for (var i = string.length - 1; i >= 0; i--) {
+    stringInvertida += string[i];
+}
+
+// EXEMPLO DE SAÍDA:
+console.log(stringInvertida);
